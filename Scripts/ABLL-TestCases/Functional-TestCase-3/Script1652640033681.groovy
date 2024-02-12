8270edf55f564895c6f3937b59b9fcbcab1819f2
Mounia Janah <89919019+AtamaWarui@users.noreply.github.com>
2024-02-12 08:29:46 -0500

    Update Script1652640033681.groovy

--- Scripts/ABLL-TestCases/Functional-TestCase-3/Script1652640033681.groovy
+++ Scripts/ABLL-TestCases/Functional-TestCase-3/Script1652640033681.groovy
@@ -21,9 +21,9 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('https://nk-gke-test.gcp.workmap.ai/#/boards/2')
 
-WebUI.setText(findTestObject('Object Repository/Page_Workmap/input_Sign in_normal_login_username (7) (1)'), 'pankajunk@gmail.com')
+WebUI.setText(findTestObject('Object Repository/Page_Workmap/input_Sign in_normal_login_username (7) (1)'), '')
 
-WebUI.setEncryptedText(findTestObject('Object Repository/Page_Workmap/input_Sign in_normal_login_password (7) (1)'), 'Piu45nT0PQ8=')
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_Workmap/input_Sign in_normal_login_password (7) (1)'), '')
 
 WebUI.click(findTestObject('Object Repository/Page_Workmap/button_Log in (7) (1)'))
 
